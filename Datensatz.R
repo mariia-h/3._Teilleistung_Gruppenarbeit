d1d3223a7e2dfad73bd45982511d0d17d24965cf
dalymoez <76786031+dalymoez@users.noreply.github.com>
2021-02-17 21:07:25 +0100

    Datensatz Update
    i updated the Datensatz  R-skript and added the documentation and created the .scv file

--- Datensatz.R
+++ Datensatz.R
@@ -3,10 +3,14 @@
 
 
 ## Alter: 
+
+    #wir haben einfach einen Vektor erstellet mit den Alter, sodass sie normal verteilt sind.
 alter = round(rnorm(100,sd= 2,mean = 25))
 
 
 ##Studienfach: 
+   #Analoge zum Alter aber wir haben statistik und Data Science mehrmals geschrieben um "haufiger"
+#zu simulieren
 fach = c(rep("Statistik",5),rep("Data Science",5),"Mathe",
                 rep("Informatik",3))
 studienfach = sample(fach,100,replace = TRUE)
@@ -18,24 +22,31 @@ studienfach = sample(fach,100,replace = TRUE)
 #                         - Statistik
 #                         - Data Science
 #                         - Informatik 
+
+
+#Normalerweiser magen die Studenten, derjenigen mathe studieren, deswegen hörere Anzahlen haufiger
+#vorkommen. Dann nutzen wir dies neu Vektor um das Interesse an Mathe auf die Leute verteilen, die 
+#mathe  studieren 
 mathe_mathe = c(rep(1,1),rep(1,2),rep(3,3),rep(4,6),rep(5,7),rep(6,7),rep(7,6))
 mathe_mathe.final = sample(mathe_mathe,size = length(which(studienfach =="Mathe")),
                replace = TRUE)
 
-
+#Idr magen die Informatiker*innen Mathe veniger als derjenigen, die Mathe studieren.
 mathe_info = c(rep(7,1), rep(6,1), rep(5,3), rep(4,6), rep(3,7), rep(2,7),rep(1,6))
 mathe_info.final = sample(mathe_info,size = length(which(studienfach =="Informatik")),
                replace = TRUE)
 
+# Statistik ist analog verteilt
 mathe_stat = c(rep(1,2),rep(1,3),rep(3,4),rep(4,7),rep(5,6),rep(6,6),rep(7,5))
 mathe_stat.final = sample(mathe_stat,size = length(which(studienfach =="Statistik")),
                replace = TRUE)
 
+# Data sience ist auch analog verteilt
 mathe_ds = c(rep(1,2),rep(1,3),rep(3,4),rep(4,7),rep(5,6),rep(6,6),rep(7,5))
 mathe_ds.final = sample(mathe_stat,size = length(which(studienfach =="Data Science")),
                           replace = TRUE)
 
-#final: 
+#Am Ende bekommen wir dies Vektor.
 Interesse_an_Mathematik = rep(0,100)
 Interesse_an_Mathematik[which(studienfach=="Mathe")] = mathe_mathe.final
 Interesse_an_Mathematik[which(studienfach=="Informatik")] = mathe_info.final
@@ -45,6 +56,7 @@ Interesse_an_Mathematik[which(studienfach=="Statistik")] = mathe_stat.final
 
 ## Interesse_an_programmieren
 
+#Analog zum Inderesse_an_mathematik
 prog_mathe = c(rep(7,1), rep(6,1), rep(5,3), rep(4,6), rep(3,7), rep(2,7),rep(1,6))
 prog_mathe.final = sample(prog_mathe,size = length(which(studienfach =="Mathe")),
                            replace = TRUE)
@@ -82,4 +94,4 @@ datensatz=data.frame(Alter = alter ,Studienfach = studienfach ,
                      Interesse_an_Programmieren = Interesse_an_Programmieren,
                      Mathe_LK = mathe_LK)
 
-## Bleibt noch die Dokumentation: er
+write.csv(datensatz,'Datensatz.csv')
