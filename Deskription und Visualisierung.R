c0f8f8a467479088141323822a4acfac5b60787f
Ayoubhbz <79419835+Ayoubhbz@users.noreply.github.com>
2021-02-28 18:48:02 +0100

    Update Deskription und Visualisierung.R

--- Deskription und Visualisierung.R
+++ Deskription und Visualisierung.R
@@ -17,8 +17,8 @@ lagemasse(Datensatz)
 #[1] 2.733854e+137
 
 #arithmetisches Mittel vom Alter zeigt uns ,dass die Studierende im Durchschnitt 25 jahre alt sind
-# Außerdem das Modus vom Interesse an Mathematik betregt 5,das heißt ,dass die Studenten ein überdurchschnittliches Interesse an Mathematik haben
-# und durch die geometrisches Mittel vom Interesse an Prog sieht man ,dass es ein durchschnittliche Veränderungsrate von 4.311268e+137 gibt
+# AuÃŸerdem das Modus vom Interesse an Mathematik betregt 5,das heiÃŸt ,dass die Studenten ein Ã¼berdurchschnittliches Interesse an Mathematik haben
+# und durch die geometrisches Mittel vom Interesse an Prog sieht man ,dass es ein durchschnittliche VerÃ¤nderungsrate von 4.311268e+137 gibt
 
 
 bfunktion(Datensatz)
@@ -27,33 +27,33 @@ bfunktion(Datensatz)
 #$`Modalwert von Studienfach sowie des entsprechenden Fach:`
 #[1] "Data Science" "Data Science"
 
-#$`Für die Interesse an Mathematik:`
+#$`FÃ¼r die Interesse an Mathematik:`
 #Modalwert    Median 
 #5         4 
 
-#$`Für die Interesse an Programmieren:`
+#$`FÃ¼r die Interesse an Programmieren:`
 #Modalwert    Median 
 #4.0       4.5 
 
 
 #hier sieht man deutlich ,dass das am haufigsten vorkommenden Studienfach "Data Science" ist 
 # die Mehrheit der Studenten weisen ein durschnittliche Interesse an Mathe und Programierung auf, das kann man in die Modalwerte "5" ablesen.
-# und durch Der Median sieht man, dass "4" das Wert ist, das die Datenreihe  bei der Interesse an Mathematik halbiert , das gleiche gilt auch Für die Interesse an Programmieren
-#sodass eine Hälfte der Daten unterhalb und die andere Hälfte oberhalb des Medians in der geordneten Reihe liegt.
+# und durch Der Median sieht man, dass "4" das Wert ist, das die Datenreihe  bei der Interesse an Mathematik halbiert , das gleiche gilt auch FÃ¼r die Interesse an Programmieren
+#sodass eine HÃ¤lfte der Daten unterhalb und die andere HÃ¤lfte oberhalb des Medians in der geordneten Reihe liegt.
 
 
 zusammenhangsmasse(Datensatz)
 #Der Kontingenzkoeffizient kann Werte zwischen 0 und 1 annehmen.
 #Dabei bedeutet 0, dass es keinen Zusammenhang zwischen den beiden Merkmalen gibt, 
-#und 1, dass es einen vollständigen Zusammenhang gibt.
+#und 1, dass es einen vollstÃ¤ndigen Zusammenhang gibt.
 # deswegen kann mann hier ablesen, dass zwischen Studienfach und die Tatsache, dass Person Mathe-LK hatte kein Zusammenhang besteht
 #und es ganz eine schwach bis mittlerer Zusammenhangzwischen Interesse an Mathematik und Studienfach besteht.
-# Für die Zusammengang zwischen Interesse an Programmieren und Studienfach weisen Kontingenzindexen aud unterschidliche stärke von die Zussemmenhänge, 
-#also von ein ganze schwacher bis ein ganz starker Zusammenhang. deshalb kann mann hier keinen klaren Aussage abschließen .
+# FÃ¼r die Zusammengang zwischen Interesse an Programmieren und Studienfach weisen Kontingenzindexen aud unterschidliche stÃ¤rke von die ZussemmenhÃ¤nge, 
+#also von ein ganze schwacher bis ein ganz starker Zusammenhang. deshalb kann mann hier keinen klaren Aussage abschlieÃŸen .
 # dazu kann man hier auch ablesen , dass es keine Korellation zwischen Interesse an Mathematik und Interesse an Programmieren gibt
 
 
-#dfunktion(Datensatz)
+dfunktion(Datensatz)
 
 #Das Ergebnis: 
 #[[1]]
@@ -77,9 +77,9 @@ zusammenhangsmasse(Datensatz)
 
 #Der Korrelationskoeffizient kann Werte zwischen -1 und 1 annehmen, wobei ein Korrelationskoeffizient von 0 bedeutet, 
 #dass kein Zusammenhang zwischen beiden Variablen existiert.Ein Korrelationskoeffizient von +1 beschreibt einen perfekten positiven Zusammenhang
-#zwischen beiden Variablen, während eine Korrelation von -1 einen perfekten negativen (inversen) Zusammenhang (Antikorrelation) beschreibt.
+#zwischen beiden Variablen, wÃ¤hrend eine Korrelation von -1 einen perfekten negativen (inversen) Zusammenhang (Antikorrelation) beschreibt.
 
-# da hier 0.02377276 rauskommt, heißt das , dass hier fast gar kein linearen Zusammenhang zwischen Alter und die Interesse an Mathe gibt.
+# da hier 0.02377276 rauskommt, heiÃŸt das , dass hier fast gar kein linearen Zusammenhang zwischen Alter und die Interesse an Mathe gibt.
 
 
 
@@ -98,11 +98,11 @@ kategorien(Datensatz)
 
 
 
-#in erste Tabelee sieht mann ,dass für Mathe Studierend :  2 Studenten (50% der studierende) zeigt ein mittelere Interese an Mathematik,
-#während 2 studierende (50% der studierende) zeigen hoch Interesse an Mathe. Und keiner hat niedrige Interesse an Mathe.
+#in erste Tabelee sieht mann ,dass fÃ¼r Mathe Studierend :  2 Studenten (50% der studierende) zeigt ein mittelere Interese an Mathematik,
+#wÃ¤hrend 2 studierende (50% der studierende) zeigen hoch Interesse an Mathe. Und keiner hat niedrige Interesse an Mathe.
 
-#in zweite Tabelee sieht mann ,dass für Informatik Studierend : nur 4 Student (12.5% der studierende) zeigt ein niedrige Interese an Programmieren,
-#während 13 studierende (40.625% der studierende) zeigen mittel Interesse an Programmieren. Und 15 (46.875% der studierende) studierende haben hoch Interesse an Programmieren.
+#in zweite Tabelee sieht mann ,dass fÃ¼r Informatik Studierend : nur 4 Student (12.5% der studierende) zeigt ein niedrige Interese an Programmieren,
+#wÃ¤hrend 13 studierende (40.625% der studierende) zeigen mittel Interesse an Programmieren. Und 15 (46.875% der studierende) studierende haben hoch Interesse an Programmieren.
 
 
 visualisation(Studienfach,Interesse_an_Mathematik,Mathe_LK)
